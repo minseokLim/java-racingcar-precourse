228736b0245da952fb75a6b9b37d9f8798ad2275
minseokLim <mslim8803@naver.com>
2021-10-11 22:43:06 +0900

    refactor(DiceResult): isMovingForward() 메서드 추가
    enum 객체에 직접 접근하지 않고 메서드를 통해 전진 여부를 판단하도록 수정

--- src/main/java/racinggame/model/enums/DiceResult.java
+++ src/main/java/racinggame/model/enums/DiceResult.java
@@ -1,5 +1,9 @@
 package racinggame.model.enums;
 
 public enum DiceResult {
-	MOVING_FORWARD, STOP
+	MOVING_FORWARD, STOP;
+
+	public boolean isMovingForward() {
+		return this == MOVING_FORWARD;
+	}
 }
